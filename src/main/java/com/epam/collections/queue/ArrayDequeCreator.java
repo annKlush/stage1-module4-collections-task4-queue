97e3453ab8ebf0f657e69ec0bf56a962ca4f0848
annKlush <109134951+annKlush@users.noreply.github.com>
2023-07-17 22:10:36 +0300

    com

--- src/main/java/com/epam/collections/queue/ArrayDequeCreator.java
+++ src/main/java/com/epam/collections/queue/ArrayDequeCreator.java
@@ -1,10 +1,31 @@
 package com.epam.collections.queue;
 
 import java.util.ArrayDeque;
+import java.util.List;
 import java.util.PriorityQueue;
 import java.util.Queue;
 
 public class ArrayDequeCreator extends PriorityQueue<String> {
     public ArrayDeque<Integer> createArrayDeque(Queue<Integer> firstQueue, Queue<Integer> secondQueue) {
+
+        ArrayDeque<Integer> arrayDeque = new ArrayDeque<>();
+        arrayDeque.offer(firstQueue.poll());
+        arrayDeque.offer(firstQueue.poll());
+
+
+        arrayDeque.offer(secondQueue.poll());
+        arrayDeque.offer(secondQueue.poll());
+        while (!firstQueue.isEmpty() && !secondQueue.isEmpty()) {
+            firstQueue.offer(arrayDeque.pollLast());
+            arrayDeque.offer(firstQueue.poll()); // Add the top card from Player 1's queue to the ArrayDeque
+            arrayDeque.offer(firstQueue.poll()); // Add the next card from Player 1's queue to the ArrayDeque
+
+            secondQueue.offer(arrayDeque.pollLast());
+            arrayDeque.offer(secondQueue.poll()); // Add the top card from Player 2's queue to the ArrayDeque
+            arrayDeque.offer(secondQueue.poll()); // Add the next card from Player 2's queue to the ArrayDeque
+
+        }
+
+        return arrayDeque;
     }
 }
--- src/main/java/com/epam/collections/queue/DishOrderDeterminer.java
+++ src/main/java/com/epam/collections/queue/DishOrderDeterminer.java
@@ -1,8 +1,34 @@
 package com.epam.collections.queue;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 public class DishOrderDeterminer {
     public List<Integer> determineDishOrder(int numberOfDishes, int everyDishNumberToEat) {
+        List<Integer> dishOrder = new ArrayList<>();
+        Queue<Integer> queue = new LinkedList<>();
+
+        // Initialize the queue with dish numbers
+        for (int i = 1; i <= numberOfDishes; i++) {
+            queue.offer(i);
+        }
+
+        // Process the dishes until the queue is empty
+        while (!queue.isEmpty()) {
+            // Eat everyDishNumberToEat dish
+            for (int i = 1; i < everyDishNumberToEat; i++) {
+                // Remove and add the dish back to the queue
+                int dish = queue.poll();
+                queue.offer(dish);
+            }
+
+            // Eat the current dish and remove it from the queue
+            int dish = queue.poll();
+            dishOrder.add(dish);
+        }
+
+        return dishOrder;
     }
 }
